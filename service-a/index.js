7842177791a3bbd1da20f9d8b22bbfa43fd30197
sagi-confetti <97825390+sagi-confetti@users.noreply.github.com>
2022-04-14 00:44:00 +0300

    added communication between microservices

--- service-a/index.js
+++ service-a/index.js
@@ -1,12 +1,27 @@
-import express from 'express';
+import express from "express";
+import axios from "axios";
 
 const app = express();
 
 app.listen(3000, () =>
-  console.log('Service-a listening on internal port 3000...'),
+  console.log("Service-a listening on internal port 3000...")
 );
 
-app.get('/', (req, res) => {
-    res.send('Hello from service-a!');
+app.get("/", (req, res) => {
+  res.send("Hello from service-a!");
 });
-  
+
+app.get("/message", (req, res) => {
+  console.log("/message on service-a called");
+  res.send({ message: "This is a message from service-a" });
+});
+
+app.get("/message-to-service-b", async (req, res) => {
+  try {
+    // http://<container_hostname>:<internal_port>/
+    const response = await axios.get("http://service_b:3000/message");
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
--- service-b/index.js
+++ service-b/index.js
@@ -1,12 +1,27 @@
-import express from 'express';
+import express from "express";
+import axios from "axios";
 
 const app = express();
 
 app.listen(3000, () =>
-  console.log('Service-b listening on internal port 3000...'),
+  console.log("Service-b listening on internal port 3000...")
 );
 
-app.get('/', (req, res) => {
-    res.send('Hello from service-b!');
+app.get("/", (req, res) => {
+  res.send("Hello from service-b!");
 });
-  
+
+app.get("/message", (req, res) => {
+  console.log("/message on service-b called");
+  res.send({ message: "This is a message from service-b" });
+});
+
+app.get("/message-to-service-a", async (req, res) => {
+  try {
+    // http://<container_hostname>:<internal_port>/
+    const response = await axios.get("http://service_a:3000/message");
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
